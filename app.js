184a210dffc2716d65311f003145c5234a8285cd
Shin <niconico@Shinsuke-MacBook.local>
2023-01-26 02:23:17 +0900

    server create

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+server.listen(PORT, ()=>{
+  console.log("listening on *:3000");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(express.static('public'));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+io.on('connection', function(socket){
+  socket.on('myMessage', function(msg){
+      io.emit('myMessage', msg);
+  });
 });
 
-module.exports = app;
+io.on('connection', function(socket){
+  socket.on('otherMessage', function(msg){
+      io.emit('otherMessage', msg);
+  });
+});
+
+
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,6 @@
+
+const socket = io();
+
 // 共通する要素のDOMを取得
 const btn = document.getElementById('button');
 const chat = document.getElementById('chat');
@@ -10,44 +13,49 @@ let isMyself = true;
 // 自分が送信する際の処理
 function mySend() {
   if (textarea.value !== "") {
-      // textareaに入力されたテキストを取得
-  let Message = textarea.value;
-  
-  // #chatの子要素になるdiv要素を生成しクラスを付与
-  const MessageBox = document.createElement('div');
-  MessageBox.classList.add('MyMessageBox');
+    // textareaに入力されたテキストを取得
+    let Message = textarea.value;
 
-  // #chat > .MyMessageBoxの子要素になるdivを生成し、クラスを付与
-  const time = document.createElement('div');
-  time.classList.add('time');
+    // #chatの子要素になるdiv要素を生成しクラスを付与
+    const MessageBox = document.createElement('div');
+    MessageBox.classList.add('MyMessageBox');
 
-  // .timeに現在の時間を表示する関数を代入
-  time.textContent = showTime() + " " + "Me";
+    // #chat > .MyMessageBoxの子要素になるdivを生成し、クラスを付与
+    const time = document.createElement('div');
+    time.classList.add('time');
 
-  // #chat > .MyMessageBoxの子要素になるdivを生成
-  const sentMessage = document.createElement('div');
+    // .timeに現在の時間を表示する関数を代入
+    time.textContent = showTime() + " " + "Me";
 
-  // .MyMessageBox > sentMessageの子要素になるpタグを生成し、クラスを付与
-  const myMessage = document.createElement('p');
-  myMessage.classList.add('myMessage');
+    // #chat > .MyMessageBoxの子要素になるdivを生成
+    const sentMessage = document.createElement('div');
 
-  // textareaに入力されたテキストをpタグに格納
-  myMessage.textContent = Message;
+    // .MyMessageBox > sentMessageの子要素になるpタグを生成し、クラスを付与
+    const myMessage = document.createElement('p');
+    myMessage.classList.add('myMessage');
 
-  // 各要素を格納
-  sentMessage.appendChild(myMessage);
-  MessageBox.appendChild(time);
-  MessageBox.appendChild(sentMessage);
-  chat.appendChild(MessageBox);
+    // ページの最下部に画面を固定
+    MessageBox.scrollIntoView(false);
 
-  // 送信ボタンをクリックしたらtextareaを空にする
-  textarea.value = "";
+    socket.emit("myMessage", Message);
 
-  // 交互に送信するために変数を変更
-  isMyself = false;
+    socket.on('myMessage', function(msg){
+      // textareaに入力されたテキストをpタグに格納
+      myMessage.textContent = msg;
 
-  // ページの最下部に画面を固定
-  MessageBox.scrollIntoView(false);
+      // 各要素を格納
+      sentMessage.appendChild(myMessage);
+      MessageBox.appendChild(time);
+      MessageBox.appendChild(sentMessage);
+      chat.appendChild(MessageBox);
+
+      // 送信ボタンをクリックしたらtextareaを空にする
+      textarea.value = "";
+
+      // 交互に送信するために変数を変更
+      isMyself = false;
+    });
+
   }
 }
 
@@ -57,7 +65,7 @@ function otherSend() {
   if (textarea.value !== "") {
       // textareaに入力されたテキストを取得
   let Message = textarea.value;
-  
+
   // #chatの子要素になるdiv要素を生成しクラスを付与
   const MessageBox = document.createElement('div');
   MessageBox.classList.add('OtherMessageBox');
@@ -76,8 +84,14 @@ function otherSend() {
   const otherMessage = document.createElement('p');
   otherMessage.classList.add('otherMessage');
 
+  // ページの最下部に画面を固定
+  MessageBox.scrollIntoView(false);
+
+  socket.emit("otherMessage", Message);
+
+  socket.on('otherMessage', function(msg){
   // textareaに入力されたテキストをpタグに格納
-  otherMessage.textContent = Message;
+  otherMessage.textContent = msg;
 
   // 各要素を格納
   sentMessage.appendChild(otherMessage);
@@ -90,9 +104,7 @@ function otherSend() {
 
   // 交互に送信するために変数を変更
   isMyself = true;
-
-  // ページの最下部に画面を固定
-  MessageBox.scrollIntoView(false);
+  });
   }
 }
 
